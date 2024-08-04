70bb15ac87a1fc8825e5d54fa93120f0cfdf7bfa
SomeRandomNiko <perathonerniko@gmail.com>
2024-08-04 19:30:32 +0200

    added eslint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json", // Path to your tsconfig.json
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      "@typescript-eslint/no-floating-promises": "error", // or "warn" if you prefer
+    },
+  },
+];
